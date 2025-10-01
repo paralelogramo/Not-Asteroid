5658a9d717792ec2ab6c403e6fb086875196be45
Vicente Rojas <vrojasaranda@gmail.com>
2025-10-01 10:52:05 -0300

    feat: Add home view component to app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Home } from "./views/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
