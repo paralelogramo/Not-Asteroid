581ca37b292422e7d3dddf2872f115c0edffcb36
Vicente Rojas <vrojasaranda@gmail.com>
2025-10-01 10:35:26 -0300

    chore: Add libraries & config files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                audiowide: ['"Audiowide"', 'cursive'],
+            },
+        },
+    },
+    plugins: [],
+}
