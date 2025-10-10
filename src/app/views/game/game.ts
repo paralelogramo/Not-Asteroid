dd1d88600ef67bcedc2274337a0ff9344bbfa25f
Vicente Rojas <vrojasaranda@gmail.com>
2025-10-09 22:53:41 -0300

    feat: Add game view component logic

--- src/app/views/game/game.ts
+++ src/app/views/game/game.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import * as PIXI from 'pixi.js';
+import Matter from 'matter-js';
+import { GameService } from './game.service';
 
 @Component({
-  selector: 'app-game',
-  imports: [],
-  templateUrl: './game.html',
-  styleUrl: './game.css'
+    selector: 'app-game',
+    imports: [],
+    templateUrl: './game.html',
+    styleUrl: './game.css'
 })
 export class Game {
+    @ViewChild('gameContainer', { static: true }) gameContainer!: ElementRef<HTMLDivElement>;
+
+    constructor(private gameService: GameService) { }
+
+    async ngAfterViewInit(): Promise<void> {
+        await this.gameService.initializeGame(this.gameContainer.nativeElement);
+    }
+
+    ngOnDestroy(): void {
+        this.gameService.destroy();
+    }
+
 
 }
